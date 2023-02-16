ef3998dae2f2f35f8811a0c60862cfcb3f4ed1ad
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-16 17:21:43 +0200

    Add writeFile

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 "use strict";
 
-const fs = require("fs");
-//const fs = require("fs/promises"); //readFile
+const fs = require("fs/promises");
 
-fs.readFile('./test.txt', {encoding:'utf-8'}, (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+fs.readFile("./test.txt", "utf-8")
+  .then((data) => {
+    console.log(data);
+    fs.writeFile("./newTest.txt", data + "\nLOREM!!!", "utf-8");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
