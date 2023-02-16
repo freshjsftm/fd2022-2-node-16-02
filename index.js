9319c17cf73c02e53a22633fa0102703bc944d72
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-16 16:12:01 +0200

    Add how work resolving

--- index.js
+++ index.js
@@ -2,10 +2,20 @@
 const {MyMath} = require('./MyMath');
 const {Component} = require('./Component');
 
-//console.log(MyMath)
-
 const result = MyMath.sum(4, 8);
 console.log("result = ", result);
 
 const component1 = new Component();
 component1.render();
+
+// require
+// resolving -> loading -> wrapping -> evaluation -> caching
+
+// resolving:
+// 1 - core modules
+// 2 - filename.js | filename.json
+// 3 - directory 
+//   3.1 - directory/package.json -> "main"
+//   3.2 - directory/index.js | directory/index.json
+// 4 - node_modules
+// 5 - throw new Error()
